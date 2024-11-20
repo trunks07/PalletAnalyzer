4def23dbc2b49aa05835feb29dec4847939d7a07
trunks07 <carlog@mkthemedattractions.com.ph>
2024-11-20 08:00:25 +0800

    Fix bug in catalogs lookp up

--- app/routers/Catalog.py
+++ app/routers/Catalog.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, APIRouter, HTTPException, Request, status
 from fastapi.responses import JSONResponse
 
 from app.services.CatalogService import CatalogService
+from app.services.BusinessCentralService import BusinessCentral as BusinessCentralService
 
 router = APIRouter()
 
@@ -14,6 +15,19 @@ async def getCatalogs():
 
         status_code = status.HTTP_200_OK
         response = {"status": status_code, "data": catalogs}
+    except HTTPException  as e:
+        status_code = status.HTTP_400_BAD_REQUEST
+        response = {"status": status_code, "error": e}
+
+    return JSONResponse(status_code=status_code, content=response)
+
+@router.get("/get-products", tags=["catalogs"])
+async def getCatalogs():
+    try:
+        products = await BusinessCentralService.getAllProducts()
+
+        status_code = status.HTTP_200_OK
+        response = {"status": status_code, "data": products}
     except HTTPException  as e:
         status_code = status.HTTP_400_BAD_REQUEST
         response = {"status": status_code, "error": e}
